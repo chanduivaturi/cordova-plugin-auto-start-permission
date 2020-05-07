b0677bb7b526055f90cc5d051708be3aa5aa23e3
chandu ivaturi <chandu.tracknix@gmail.com>
2020-05-07 15:52:55 +0530

    Added to window plugins

--- www/AutoStartPermission.js
+++ www/AutoStartPermission.js
@@ -1,27 +1,28 @@
 var exec = require('cordova/exec');
 
-var AutoStartPermission = {
-  dupli: "pluigins1",
-  isAutoStartPermissionAvailable: function() {
-    exec(function(res){}, function(err){}, "AutoStartPermission", "isAutoStartPermissionAvailable", []);
-  },
-  getAutoStartPermission : function() {
-    exec(function(res){}, function(err){}, "AutoStartPermission", "getAutoStartPermission", []);
-  }
+function AutoStartPermission() {
 }
+
+AutoStartPermission.prototype.isAutoStartPermissionAvailable = function() {
+  exec(function(res){}, function(err){}, "AutoStartPermission", "isAutoStartPermissionAvailable", []);
+};
+AutoStartPermission.prototype.getAutoStartPermission = function() {
+  exec(function(res){}, function(err){}, "AutoStartPermission", "getAutoStartPermission", []);
+};
 
 
 //AutoStartPermission.install = function () {
     if (!window.plugins) {
       window.plugins = {};
     }
-  
-    window.plugins.AutoStartPermission = AutoStartPermission;
-  
+    var autoStartPermission = new AutoStartPermission();  
     console.log('pluigins1', window.plugins.AutoStartPermission);
     console.log('pluigins2', window.plugins.AutoStartPermission.getAutoStartPermission);
     //return window.plugins.AutoStartPermission;
   //};
   
   //cordova.addConstructor(AutoStartPermission.install);
-  module.exports = AutoStartPermission
+module.exports = autoStartPermission;
+if (!window.plugins.AutoStartPermission) {
+  window.plugins.AutoStartPermission = autoStartPermission;
+}
